7e922f4ec504c94b92c8a1fc791fd364d013eae3
rxmate1 <Marius.w.larsen2@gmail.com>
2024-11-24 22:28:15 +0100

    24.11.2024

---
+++ style.cs
@@ -0,0 +1,63 @@
+       <footer id="Kontakt">
+            <section id="Logo">
+                <h3>Logo</h3>
+                <p>sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt </p>
+            </section>
+            <section id="Kontakt-oss-Snarveier">
+                <section id="Kontakt-oss">
+                    <h3>Kontakt</h3>
+                    <section>
+                        <section>
+                            <section class="ikon-footer">
+                                <i class="fa-solid fa-location-dot"></i>
+                            </section>
+                            <h4>Adresse:</h4>
+                            <p>B R A Veien 4, 1757 Halden</p>
+                        </section>
+                        <section>
+                            <section>
+                                <i class="fa-solid fa-phone"></i>
+                            </section>
+                            <h4>Telefon:</h4>
+                            <p>69 60 80 00</p>
+                        </section>
+                        <section>
+                            <section>
+                                <i class="fa-solid fa-envelope"></i>
+                            </section>
+                            <h4>E-post:</h4>
+                            <p>halden@hiof.no</p>
+                        </section>
+                        <section>
+                            <section>
+                                <i class="fa-solid fa-clock"></i>
+                            </section>
+                            <h4>Åpningstider:</h4>
+                            <p>Stenger 15:00</p>
+                        </section>
+
+                    </section>
+                </section>
+                <section id="Snarveier">
+                    <h3>Snarveier</h3>
+                    <a href="#">Produkter</a>
+                    <a href="#">Kategorier</a>
+                    <a href="#">Kontakt oss</a>
+                </section>
+            </section>
+            <section id="Sosiale-medier">
+                <h3>Sosiale medier</h3>
+                <a href="https://www.instagram.com" target="_blank">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="https://www.facebook.com" target="_blank">
+                    <i class="fa-brands fa-facebook"></i>
+                </a>
+                <a href="https://www.linkedin.com" target="_blank">
+                    <i class="fa-brands fa-linkedin"></i>
+                </a>
+                <a href="https://www.pinterest.com" target="_blank">
+                    <i class="fa-brands fa-pinterest"></i>
+                </a>
+            </section>
+        </footer>
